2dfb86750e427dece86b8d8663229f782e3744bb
unknown <jyotsnashelare5@gmail.com>
2025-04-25 15:43:45 +0530

    Addition: Backend Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { getToken } from './tokenService';
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://book-vault-v6e5.onrender.com/api',
-// });
  const api = axios.create({
-   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api'
-});
+   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://book-vault-7g4z.onrender.com/api',
+ });
+ //const api = axios.create({
+   //baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api'
+//  });
 // Request interceptor
 api.interceptors.request.use(
   (config) => {
